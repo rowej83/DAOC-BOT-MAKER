071b4f2fd82745c3e554c99c1ddd761039f634b5
Jason Rowe <rowej83@gmail.com>
2013-06-20 11:06:20 -0400

    initial

--- js/app.js
+++ js/app.js
@@ -1,16 +1,43 @@
+function selectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();        
+        range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
 function Controller($scope) {
 $scope.doshow=false;
   $scope.master= {};
- 
+
+
   $scope.update = function(user) {
     $scope.master= angular.copy(user);
   };
  
-  $scope.reset = function() {
+ /** $scope.reset = function() {
     $scope.user = angular.copy($scope.master);
+  }; **/
+  
+    $scope.resetform = function() {
+    $scope.bot = angular.copy($scope.master);
   };
- 
-  $scope.reset();
+   $scope.selectall = function() {
+   //alert('happened');
+		selectText('prer');
+  };
+ // $scope.reset();
+
   
   $scope.checkshow=function(){
   if($scope.doshow){$scope.doshow=false;
